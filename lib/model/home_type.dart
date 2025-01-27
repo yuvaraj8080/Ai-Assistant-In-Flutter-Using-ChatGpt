7cca61fc4b322af79a52494554daadb14e3a1b4b
Yuvaraj Dekhane <148527838+yuvaraj8080@users.noreply.github.com>
2025-01-27 21:27:15 +0530

    This is the AI Chatbot App using the gemini chat api

--- lib/model/home_type.dart
+++ lib/model/home_type.dart
@@ -5,31 +5,27 @@ import '../screen/loadWebView.dart';
 
 
 
-enum HomeType { aiChatBot, aiImage }
+enum HomeType { aiChatBot }
 
 extension MyHomeType on HomeType {
   //title
   String get title => switch (this) {
         HomeType.aiChatBot => 'AI ChatBot',
-        HomeType.aiImage => 'AI Image Creator',
       };
 
   //lottie
   String get lottie => switch (this) {
         HomeType.aiChatBot => 'ai_hand_waving.json',
-        HomeType.aiImage => 'ai_play.json',
       };
 
   //for alignment
   bool get leftAlign => switch (this) {
         HomeType.aiChatBot => true,
-        HomeType.aiImage => false,
       };
 
   //for padding
   EdgeInsets get padding => switch (this) {
         HomeType.aiChatBot => EdgeInsets.zero,
-        HomeType.aiImage => const EdgeInsets.all(20),
 
   };
 
@@ -37,6 +33,5 @@ extension MyHomeType on HomeType {
   //for navigation
   VoidCallback get onTap => switch (this) {
         HomeType.aiChatBot => () => Get.to(() => const ChatBotFeature()),
-        HomeType.aiImage => () => Get.to(() => const LoadWebViewScreen()),
       };
 }
